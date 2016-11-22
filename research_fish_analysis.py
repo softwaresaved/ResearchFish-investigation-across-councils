6f993c278ebdb158ee14bfcaff920044ce62ec5c
Olivier PHILIPPE <olivier.philippe@gmail.com>
2016-11-22 12:00:14 +0000

    Write the import_to_csv function

--- research_fish_analysis.py
+++ research_fish_analysis.py
@@ -11,20 +11,23 @@ DATAFILENAME = "./data/Software&TechnicalProducts - ResearchFish.csv"
 def import_csv_to_dict(filename):
     """
     Importing csv into Python
+    :params: get a str of the filename
+    :return: a list() of dict()
     """
+    output_list = list()
     with open(filename,'r') as csvfile:
         reader = csv.DictReader(csvfile)
-
         for rows in reader:
-            (rows)
-    return data
+            output_list.append(rows)
+    return output_list
+    # return [rows for rows in reader]
 
 
-def main():
+def main();
     """
     Main function to run program
     """
-    dict_data = import_csv(DATAFILENAME)
+    dict_data = import_csv_to_dict(DATAFILENAME)
 
 
 if __name__ == '__main__':
