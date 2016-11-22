99bf5946624b40a9e7223d6541c44e426e96be6d
Olivier PHILIPPE <olivier.philippe@gmail.com>
2016-11-22 11:44:55 +0000

    Update function

--- research_fish_analysis.py
+++ research_fish_analysis.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import pandas
+import pandas as pd
 import csv
 
 
 DATAFILENAME = "./data/Software&TechnicalProducts - ResearchFish.csv"
 
 
-def import_csv(filename):
+def import_csv_to_dict(filename):
     """
     Importing csv into Python
     """
     with open(filename,'r') as csvfile:
-        datareader = csv.reader(csvfile)
-        for rows in datareader:
-            print(rows)
-    
+        reader = csv.DictReader(csvfile)
+
+        for rows in reader:
+            (rows)
+    return data
+
+
 def main():
     """
     Main function to run program
     """
-    import_csv(DATAFILENAME)
+    dict_data = import_csv(DATAFILENAME)
 
 
 if __name__ == '__main__':
